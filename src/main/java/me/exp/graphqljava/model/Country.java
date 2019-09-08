e0b5e5ac49be00183a22c096d821151b7a23b091
vamsh <itsvamsh@gmail.com>
2019-09-09 02:04:23 +0530

    moving to branch

--- src/main/java/me/exp/graphqljava/model/Country.java
+++ src/main/java/me/exp/graphqljava/model/Country.java
@@ -2,9 +2,13 @@ package me.exp.graphqljava.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -19,8 +23,8 @@ public class Country {
 	@Column(name="population")
 	String population;
 	
-//	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
-//	@JoinColumn(name="country_name")
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="country_name")
 	@Transient
 	List<State> states;
 	
